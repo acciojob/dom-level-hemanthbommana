7a0d2b67bc381225fe8a6b87b8b97a41dcbe874d
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-07-30 09:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+	function findElementLevel(id) {
+			let level = 0;
+			let element = document.getElementById(id);
+			
+			// Traverse up the DOM tree and count UL levels
+			while (element.parentNode) {
+				level++;
+				element = element.parentNode;
+			}
+
+			return level;
+		}
+
+		// Call the function with the ID of the target element
+		let level = findElementLevel('level');
+		alert('The level of the element is: ' + level);
+
